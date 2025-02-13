d21034cf1687e837450dae4ffaf3bd36cb3db981
Harsh Patel <harshpatel@Harshs-MacBook-Pro-5.local>
2025-02-13 13:10:25 +0530

    Updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
